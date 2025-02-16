5e3bd94a01743d2fba0cc51fba3be8b3199e7e7f
Vadim <zyuskovv@gmail.com>
2025-02-16 21:20:12 +0300

    streams completed

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import * as fs from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
+const pipelineAsync = promisify(pipeline);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const stream = fs.createReadStream(
+      "src/streams/files/fileToRead.txt",
+      "utf-8"
+    );
+    await pipelineAsync(stream, process.stdout);
+
+    console.log("File reading completed.");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-await read();
+read();
