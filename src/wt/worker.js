3134075d0be6a130b9e17e4c274e5202f6ee6793
Vadim <zyuskovv@gmail.com>
2025-02-16 21:30:09 +0300

    wt completed

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import fs from "fs";
+const write = async () => {
+  const stream = fs.createWriteStream("src/streams/files/fileToWrite.txt");
+  process.stdin.pipe(stream);
+  await new Promise((resolve) => process.stdin.on("end", resolve));
+};
+await write();
+import { workerData, parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = async () => {
+  try {
+    const result = await nthFibonacci(workerData);
+    parentPort.postMessage(result);
+  } catch (error) {
+    parentPort.postMessage(null);
+  }
 };
 
 sendResult();
